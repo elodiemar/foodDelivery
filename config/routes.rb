8c5ea4fa2b11e4a1f76ea5e367d28cffd3f67f76
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-06-20 14:05:32 +0200

    controllers ready

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :meals
+  end
+
 end
--- db/migrate/20220620113751_create_meals.rb
+++ db/migrate/20220620113751_create_meals.rb
@@ -3,6 +3,7 @@ class CreateMeals < ActiveRecord::Migration[7.0]
     create_table :meals do |t|
       t.string :name
       t.integer :price
+      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
